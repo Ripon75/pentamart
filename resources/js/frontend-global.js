27a234ee17272fac4a24294feb631030c1bb5ed7
Saikat724 <saikathosen444@gmail.com>
2023-09-24 23:34:54 +0600

    ok

--- resources/js/frontend-global.js
+++ resources/js/frontend-global.js
@@ -1,11 +1,12 @@
-import Swal from 'sweetalert2'
+import Swal from "sweetalert2";
 
 const __ls_PreviousURLKey = "previous_url";
 
 window.__debounce = function (func, wait, immediate) {
     var timeout;
     return function () {
-        var context = this, args = arguments;
+        var context = this,
+            args = arguments;
         var later = function () {
             timeout = null;
             if (!immediate) func.apply(context, args);
@@ -15,7 +16,7 @@ window.__debounce = function (func, wait, immediate) {
         timeout = setTimeout(later, wait);
         if (callNow) func.apply(context, args);
     };
-}
+};
 
 // window.__showNotification = function(type, message, timeout = 2500, layout = 'top') {
 //     return new Noty({
@@ -32,39 +33,44 @@ window.__debounce = function (func, wait, immediate) {
 //     }).show();
 // }
 
-window.__cartItemCount = function() {
-    axios.get('/cart/items/count')
-    .then(function (response) {
-        cartCount.text(response.data);
-        if (cartCount.text() > 0) {
-            $('.cart-dev').show();
-        } else {
-            $('.cart-dev').hide();
-        }
-    })
-    .catch(function (error) {
-        console.log(error);
-    });
-}
+window.__cartItemCount = function () {
+    axios
+        .get("/cart/items/count")
+        .then(function (response) {
+            cartCount.text(response.data);
+            if (cartCount.text() > 0) {
+                $(".cart-dev").show();
+            } else {
+                $(".cart-dev").hide();
+            }
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+};
 
-window.__savePreviousURL = function(url) {
+window.__savePreviousURL = function (url) {
     localStorage.setItem(__ls_PreviousURLKey, url);
-}
+};
 
-window.__getPreviousURL = function() {
+window.__getPreviousURL = function () {
     return localStorage.getItem(__ls_PreviousURLKey);
-}
+};
 
-
-window.__redirectPreviousURL = function() {
+window.__redirectPreviousURL = function () {
     const purl = __getPreviousURL();
     if (purl) {
         location.href = purl;
         localStorage.removeItem(__ls_PreviousURLKey);
     }
-}
+};
 
-window.__showNotification = function(type, message, timer = 3000, layout = 'top-end') {
+window.__showNotification = function (
+    type,
+    message,
+    timer = 10000,
+    layout = "top-end"
+) {
     const Toast = Swal.mixin({
         toast: true,
         position: layout,
@@ -72,14 +78,13 @@ window.__showNotification = function(type, message, timer = 3000, layout = 'top-
         timer: timer,
         timerProgressBar: true,
         didOpen: (toast) => {
-            toast.addEventListener('mouseenter', Swal.stopTimer)
-            toast.addEventListener('mouseleave', Swal.resumeTimer)
-        }
-    })
+            toast.addEventListener("mouseenter", Swal.stopTimer);
+            toast.addEventListener("mouseleave", Swal.resumeTimer);
+        },
+    });
 
     Toast.fire({
-      icon: type,
-      title: message
-    })
-}
-
+        icon: type,
+        title: message,
+    });
+};
